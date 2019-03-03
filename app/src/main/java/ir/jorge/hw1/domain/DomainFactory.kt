5013fd2be151446768a76dd23ba4db38fc1438f3
zahra <hosseini.simurg@gmail.com>
2019-03-03 14:44:46 +0330

    1397/12/12 not tested but completed

--- app/src/main/java/ir/jorge/hw1/domain/DomainFactory.kt
+++ app/src/main/java/ir/jorge/hw1/domain/DomainFactory.kt
@@ -1,16 +1,17 @@
 package ir.jorge.hw1.domain
 
+import android.content.Context
 import ir.jorge.hw1.system.FileSystem
 
 interface DomainFactory {
-    fun newMessageController(uiRunner: UIRunner, fileSystem: FileSystem): MessageController
+    fun newMessageController(uiRunner: UIRunner, fileSystem: FileSystem, context: Context): MessageController
     fun newNotificationCenter(): NotificationCenter
 }
 
 private object DomainFactoryImpl : DomainFactory {
-    override fun newMessageController(uiRunner: UIRunner, fileSystem: FileSystem): MessageController =
+    override fun newMessageController(uiRunner: UIRunner, fileSystem: FileSystem, context: Context): MessageController =
             MessageControllerImpl(
-                    StorageManagerImpl(fileSystem),
+                    StorageManagerImpl(fileSystem, context),
                     ConnectionManagerImpl,
                     NotificationCenterImpl,
                     uiRunner
--- app/src/main/java/ir/jorge/hw1/domain/StorageManager.kt
+++ app/src/main/java/ir/jorge/hw1/domain/StorageManager.kt
@@ -1,22 +1,27 @@
 package ir.jorge.hw1.domain
 
+import android.content.Context
 import ir.jorge.hw1.system.FileSystem
 
 interface StorageManager : DataLoader {
     fun save(lastNumber: Int)
 }
 
-class StorageManagerImpl(private val fileSystem: FileSystem) : StorageManager {
+class StorageManagerImpl(private val fileSystem: FileSystem, private val context: Context) : StorageManager {
+    private val path = "app.storage"
+    private val key = "last number"
+
+
     override fun save(lastNumber: Int) {
-        // TODO: Don't save if lastNumber is less than number already in the file.
-        print("NOT IMPLEMENTED YET")
-        print("NOT IMPLEMENTED YET")
-        print("NOT IMPLEMENTED YET")
-        print("NOT IMPLEMENTED YET")
+        val lastSavedNumber = (fileSystem.read(context, path, key))
+        if (lastSavedNumber < lastNumber)
+            fileSystem.write(context, path, lastNumber, key)
     }
 
     override fun load(currentMax: Int): List<Int> {
-        // TODO: If currentMax is greater than or equal to the number in the file return empty list
+        val lastSavedNumber = (fileSystem.read(context, path, key))
+        if (lastSavedNumber <= currentMax)
+            return listOf()
         return (1..10).map { it + currentMax }
     }
 }
