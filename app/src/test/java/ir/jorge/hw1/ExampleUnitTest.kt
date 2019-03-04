5df37b1ea4b3fcdaece596da3cf7c051c82ef5b7
Iman <salvationsentinel@yahoo.com>
2019-03-04 18:47:37 +0330

    UnitTest Added for connectionManager

--- app/src/test/java/ir/jorge/hw1/ExampleUnitTest.kt
+++ app/src/test/java/ir/jorge/hw1/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package ir.jorge.hw1
 
+import ir.jorge.hw1.domain.ConnectionManagerImpl
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -11,7 +12,14 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun ConnManTest() {
+        val cm = ConnectionManagerImpl
+        val result = cm.load(10)
+        assertEquals(10, result.size)
+        for (i in 0 until 10){
+            assertEquals(i+10+1,result[i])
+        }
+        println(this.javaClass.canonicalName+" TEST SUCCESS!")
     }
+    
 }
